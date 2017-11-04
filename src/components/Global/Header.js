43b42215929d17319fe78e2c65210eadfe0f8689
William Collins <wcurtiscollins@gmail.com>
2017-11-03 23:03:20 -0400

    add favicons and logo with name

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -47,7 +47,7 @@ const Header = (props) => {
 
         <Link className="navbar-brand" to="/">
           <span className="sr-only">EthicalTree</span>
-          <img className="ml-4" src="/assets/images/logo/logo-48x48.png" alt="EthicalTree Logo" />
+          <img className="ml-4 mr-2" src="/assets/images/logo/logo-full-64.png" alt="EthicalTree Logo" />
         </Link>
 
         {hasSearch &&
@@ -76,7 +76,7 @@ const Header = (props) => {
                 <DropdownToggle nav caret>
                   <AccountIcon email={session.user.email} />
                 </DropdownToggle>
-                <DropdownMenu right className="mt-2">
+                <DropdownMenu right className="mt-3">
                   {session.user.admin &&
                     <DropdownItem onClick={e => { history.push('/admin/users') }}>
                       Admin
