bff8f14c068c9ee863663f1126e8d3a6146ae79a
William Collins <wcurtiscollins@gmail.com>
2018-01-18 23:04:40 -0500

    add smartlook

---
+++ public/plugins/smartlook.js
@@ -0,0 +1,6 @@
+window.smartlook||(function(d) {
+var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+})(document);
+smartlook('init', '73b65a86e3d8c875a7c36e2d65962802d57c60eb');
