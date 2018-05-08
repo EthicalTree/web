d6f41ed9b9785a2d399a99d27b981185371e7766
William Collins <wcurtiscollins@gmail.com>
2018-05-07 20:13:47 -0400

    remove ununed import

--- src/components/CuratedList/CuratedList.js
+++ src/components/CuratedList/CuratedList.js
@@ -2,7 +2,6 @@ import './CuratedList.css'
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 import { Result } from '../Search/Result'
