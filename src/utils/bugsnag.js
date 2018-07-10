63caed78f6b24877c48c4286a6d3bc9490c270da
William Collins <wcurtiscollins@gmail.com>
2018-07-09 20:48:46 -0400

    add bugsnag

---
+++ src/utils/bugsnag.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
+
+const client = bugsnag({
+  apiKey: process.env.REACT_APP_BUGSNAG_KEY,
+  releaseStage: process.env.REACT_APP_ENVIRONMENT,
+  notifyReleaseStages: ['production']
+})
+
+export const assignBugsnagUser = user => {
+  client.user = user
+}
+
+export default client.use(createPlugin(React))
