3ea7ed08db61fda8fa78066eda08936f0f5c9982
Calvin Collins <calvinkcollins@gmail.com>
2018-07-11 07:40:48 -0400

    change listings to listing more stuff that I missed

--- src/containers/Listing/Listing.js
+++ src/containers/Listing/Listing.js
@@ -227,7 +227,7 @@ const ListingInfo = (props) => {
         <TabPane tabId="location">
           <ListingMap
             onClickLocationEdit={props.onClickLocationEdit}
-            locations={listing.locations}
+            location={listing.location}
             canEdit={hasPermission('update', listing)}
             dispatch={dispatch}
           />
