ede37190a36884a3312d5722c2240a43174e3777
Calvin Collins <calvinkcollins@gmail.com>
2018-10-08 14:15:29 -0400

    ET-408 fix operating hours for DST (#23)
    * fix hours so that they work with edge cases.
    Also added tests to test normal operation of hours and some edge cases.
    Also fixed the main app test.
    
    * this needs to go along with an api migration. It now will store the
    times as a generic time, which can then be parsed at runtime through the
    locations timezone.
    
    * fix to parse hours from the server in the listings timezone rather than
    in UTC
    
    * some refactoring and fix hours not changing when changing days
    
    * update tests and fix issue with hours that bleed into next day

--- src/models/hours.js
+++ src/models/hours.js
@@ -1,23 +1,41 @@
 import moment from 'moment-timezone'
 
+// get minuts from the moment datetime
+const minutesOfDay = m => {
+  return m.minutes() + m.hours() * 60
+}
+
+// pass in three moment datetimes and return whether or not
+// the middle time is in between the two times regardless of date
+const isBetween = (start, test, stop) => {
+  const s1 = minutesOfDay(start)
+  const s2 = minutesOfDay(stop)
+  const t = minutesOfDay(test)
+
+  if (t >= s1 && t <= s2) {
+    return true
+  } else if (s2 < s1 && t >= s1) {
+    // if the end time is less than the start, as long as it's
+    // past the start, then it's open
+    return true
+  }
+  return false
+}
+
 export const localizedDates = (operatingHours, timezone) => {
   return operatingHours.map(oh => {
-    const open = moment.tz(oh.openAt24Hour, 'HH:mm', 'UTC').tz(timezone)
-    const close = moment.tz(oh.closedAt24Hour, 'HH:mm', 'UTC').tz(timezone)
+    const open = moment.tz(oh.openAt24Hour, 'HH:mm', timezone).day(oh.day)
+    const close = moment.tz(oh.closedAt24Hour, 'HH:mm', timezone).day(oh.day)
     let result = { open, close }
-
-    result.hours = `${open.format('hh:mm a')} - ${close.format('hh:mm a')}`
     return { ...oh, ...result }
   })
 }
 
-export const getOpenCloseStatus = (hours, timezone) => {
-  const now = moment.tz(timezone)
-  const todaysHours = hours.filter(
-    h => h.day === now.format('dddd').toLowerCase()
-  )
+export const getOpenCloseStatus = (hours, timezone, now) => {
+  hours = localizedDates(hours, timezone)
+  now = now || moment.tz(timezone)
 
-  const result = todaysHours
+  const result = hours
     .map(h => {
       const openTime = h.open.clone()
       const closeTime = h.close.clone()
@@ -29,12 +47,14 @@ export const getOpenCloseStatus = (hours, timezone) => {
       const openingSoonTime = openTime.clone().add(-30, 'minutes')
       const closingSoonTime = closeTime.clone().add(-30, 'minutes')
 
-      if (now.isBetween(openingSoonTime, openTime)) {
-        return 'opening_soon'
-      } else if (now.isBetween(closingSoonTime, closeTime)) {
-        return 'closing_soon'
-      } else if (now.isBetween(openTime, closeTime)) {
-        return 'open'
+      if (openTime.day() === now.day() || closeTime.day() === now.day()) {
+        if (isBetween(openingSoonTime, now, openTime)) {
+          return 'opening_soon'
+        } else if (isBetween(closingSoonTime, now, closeTime)) {
+          return 'closing_soon'
+        } else if (isBetween(openTime, now, closeTime)) {
+          return 'open'
+        }
       }
 
       return null
