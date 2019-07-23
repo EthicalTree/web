ca3ddda3a9de14bfee473ac66e010e249d69fc6c
William Collins <wcurtiscollins@gmail.com>
2019-07-23 15:48:57 -0400

    ET-476: Remove number from contact page

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -10,11 +10,6 @@ export const ContactInfo = () => {
         <Icon iconKey="email" />
         <a href="mailto: info@ethicaltree.com">info@ethicaltree.com</a>
       </div>
-
-      <div className="d-flex">
-        <Icon iconKey="phone" />
-        613-413-0063
-      </div>
     </div>
   )
 }
